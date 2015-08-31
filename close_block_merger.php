8f6483cffb7003d687fb535fa9c78d4b507d8ec8
lukhns <lukehines1@gmail.com>
2015-08-31 18:03:32 +0100

    Adds close block merging

--- close_block_merger.php
+++ close_block_merger.php
@@ -14,12 +14,86 @@
 	class CloseBlockMerger
 	{
 		const BLOCK_DISTANCE = 1;
-	
+		
 		public static function Merge(&$textDocument, $onlyContent)
 		{
+			if(sizeof($textDocument->textBlocks) < 2)
+			{
+				return;
+			}
+			
+			$previousBlock;
+			$offset = 0;
+			
+			if($onlyContent)
+			{
+				$previousBlock = NULL;
+				$offset = 0;
+				
+				foreach ($textDocument->textBlocks as $textBlock) 
+				{
+					$offset++;
+					if($textBlock->isContent)
+					{
+						$previousBlock = $textBlock;
+						break;
+					}
+				}
+				
+				if($previousBlock == NULL)
+				{
+					return;
+				}
+			}
+			else
+			{
+				$previousBlock = $textDocument->textBlocks[0];
+				$offset = 1;
+			}
+			
+			$count = 0;
+			
 			foreach ($textDocument->textBlocks as $textBlock) 
 			{
-
+				if(!$textBlock->isContent)
+				{
+					$previousBlock = $textBlock;
+					continue;
+				}
+				
+				$blockDiff = $textBlock->offsetBlocksStart - $previousBlock->offsetBlocksEnd - 1;
+				if($blockDiff <= SELF::BLOCK_DISTANCE)
+				{
+					$validMerge = true;
+					if($onlyContent)
+					{
+						if(!$previousBlock->isContent || !$textBlock->isContent)
+						{
+							$validMerge = false;
+						}
+					}
+					
+					if($validMerge)
+					{
+						//$previousBlock->mergeBlock($textBlock);
+						unset($textDocument->textBlocks[$count]);
+						
+						//MERGE - Add to $markedForRemoval or unset($textDocument->textBlocks[$count]); 
+						/*
+							prevBlock.MergeNext(block);
+							it.Remove();
+						*/
+					}
+					else
+					{
+						$previousBlock = $textBlock;
+					}
+				}
+				else
+				{
+					$previousBlock = $textBlock;
+				}
+				$count++;
 			}
 		}
 	}
