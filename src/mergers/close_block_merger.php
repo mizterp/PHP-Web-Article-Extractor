29eef33ed83227149b478d4281a5aabe9502d68b
lukhns <lukehines1@gmail.com>
2015-09-03 18:46:55 +0100

    miscellaneous optimizations

--- src/mergers/close_block_merger.php
+++ src/mergers/close_block_merger.php
@@ -4,11 +4,10 @@
 		A PHP library to extract the primary article content of a web page.
 		
 		Based on the whitepaper 'Boilerplate detection using Shallow Text Features'
-		whitepaper http://www.l3s.de/~kohlschuetter/publications/wsdm187-kohlschuetter.pdf
-		and 'boilerpipe' by Dr. Christian Kohlschütter
-	
+		By Christian Kohlschuetter, Peter Fankhauser, Wolfgang Nejdl
+
 		Code author: Luke Hines
-		Licence: PHP Web Article Extractor is licensed under the MIT License.
+		Licence: PHP Web Article Extractor is made available under the MIT License.
 	*/
 	
 	class CloseBlockMerger
@@ -74,7 +73,7 @@
 					if($validMerge)
 					{
 						// Perform merger of this block into the previous block
-						$previousBlock->text .= "\r\n";
+						$previousBlock->text .= "\r\n\r\n";
 						$previousBlock->text .= $textBlock->text;
 						$previousBlock->numWords += $textBlock->numWords;
 						$previousBlock->numWordsInAnchorText += $textBlock->numWordsInAnchorText;
--- src/web_article_extractor.php
+++ src/web_article_extractor.php
@@ -4,11 +4,10 @@
 		A PHP library to extract the primary article content of a web page.
 		
 		Based on the whitepaper 'Boilerplate detection using Shallow Text Features'
-		whitepaper http://www.l3s.de/~kohlschuetter/publications/wsdm187-kohlschuetter.pdf
-		and 'boilerpipe' by Dr. Christian Kohlschütter
-	
+		By Christian Kohlschuetter, Peter Fankhauser, Wolfgang Nejdl
+
 		Code author: Luke Hines
-		Licence: PHP Web Article Extractor is licensed under the MIT License.
+		Licence: PHP Web Article Extractor is made available under the MIT License.
 	*/
 	
 	require 'text_document.php';
